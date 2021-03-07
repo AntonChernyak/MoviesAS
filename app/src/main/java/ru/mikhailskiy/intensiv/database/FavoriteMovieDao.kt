86c51014801d70d4c71e6f09bbea384d72d8ee77
AntonChernyak <anton767@yandex.ru>
2021-03-07 13:36:02 +0300

    FavoriteMovieDao update with RxJava

--- app/src/main/java/ru/mikhailskiy/intensiv/database/FavoriteMovieDao.kt
+++ app/src/main/java/ru/mikhailskiy/intensiv/database/FavoriteMovieDao.kt
@@ -4,22 +4,22 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
 import ru.mikhailskiy.intensiv.data.movie_details_model.MovieDetails
 
 @Dao
 interface FavoriteMovieDao {
     @Insert
-    fun saveFavoriteMovie(movie: MovieDetails)
+    fun saveFavoriteMovie(movie: MovieDetails): Completable
 
     @Delete
-    fun deleteFavoriteMovie(movie: MovieDetails)
+    fun deleteFavoriteMovie(movie: MovieDetails): Completable
 
     @Query("SELECT * FROM Favorite_Movies")
-    fun getAllFavoriteMovies(): List<MovieDetails>
-
-    @Query("DELETE FROM Favorite_Movies")
-    fun deleteAllFavoriteMovies()
+    fun getAllFavoriteMovies(): Observable<List<MovieDetails>>
 
     @Query("SELECT EXISTS (SELECT 1 FROM Favorite_Movies WHERE movie_id = :id)")
-    fun exists(id: Int): Boolean
+    fun exists(id: Int): Single<Boolean>
 }
